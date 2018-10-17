32788968875d1a4dda1789983d8ddeb5a363c90f
technology898989 <technology898989@gmail.com>
2018-10-17 15:25:46 +0500

    test lib for debugging

--- app/src/main/java/com/usec/arr/MainActivity.java
+++ app/src/main/java/com/usec/arr/MainActivity.java
@@ -3,7 +3,7 @@ package com.usec.arr;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.usec.testlib.mydebug;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mydebug.d("hello world");
+
     }
 }
