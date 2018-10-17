2513f51a691dba37ad82e174e373deade5eed74d
technology898989 <technology898989@gmail.com>
2018-10-17 14:36:31 +0500

    test lib for debugging

---
+++ testlib/src/main/java/com/usec/testlib/mydebug.java
@@ -0,0 +1,18 @@
+package com.usec.testlib;
+
+import android.util.Log;
+
+/**
+ * Created by Mazhar on 10/17/2018.
+ */
+
+public class mydebug {
+
+    public static final String TAG = "SUPER_AWSOME_APP";
+
+    public static void d(String message){
+        Log.d(TAG,message);
+
+    }
+
+}
